c56633e31240eb33fcf5690b34684eaa134b8417
FreddyJS <sebashgt@gmail.com>
2021-03-18 11:02:27 +0100

    Changed params name

--- pojo-ws/genwsdl.sh
+++ pojo-ws/genwsdl.sh
@@ -1,1 +1,9 @@
-~/axis2/bin/java2wsdl.sh -cp build/classes/ -cn calculator.CalculatorService -o build/classes/META-INF
+FILE=CalculatorService.wsdl
+if [ -f "$FILE" ]; then
+    echo "$FILE already exist!. Copying the file..."
+else 
+    echo "$FILE does not exist! Generating..."
+    ~/axis2/bin/java2wsdl.sh -cp build/classes/ -cn calculator.CalculatorService
+fi
+
+cp CalculatorService.wsdl build/classes/META-INF
